f8bf0ca65e354940559e98a0410c759962c84d34
Joe Troia <jtroia@joetroia.com>
2020-05-26 13:41:08 -0400

    implemented variable size drawing grid

--- app.js
+++ app.js
@@ -1,8 +1,42 @@
-const grid = document.getElementById("grid");
+let grid = document.getElementById("grid");
+let gridSize = 16;
 
-for(let i = 0; i < 256; i++)
-{
-    const div = document.createElement('div');
-    div.classList.add('cell');
-    grid.appendChild(div);
+function buildGrid() {
+    grid.style.setProperty('--gridSize', gridSize);
+
+    for(let i = 0; i < gridSize * gridSize; i++)
+    {
+        const cell = document.createElement('div');
+        cell.classList.add('cell');
+        cell.addEventListener('mouseenter', () => {
+            cell.style.backgroundColor = 'black';
+        });
+
+        cell.addEventListener('mouseleave', () => {
+            cell.style.backgroundColor = 'black';
+        });
+    
+        grid.appendChild(cell);
+    }
 }
+
+function clearGrid() {
+    grid.innerHTML = "";
+}
+
+const clear = document.querySelector('#clear');
+clear.addEventListener('click', () => {
+     clearGrid();
+     buildGrid();
+});
+
+const newGrid = document.querySelector('#buildNew');
+newGrid.addEventListener('click', () => {
+    clearGrid();
+    const sizeInput = document.getElementById('sizeInput');
+    gridSize = sizeInput.value;
+    console.log("gridSize: " + gridSize);
+    buildGrid();
+});
+
+buildGrid();
